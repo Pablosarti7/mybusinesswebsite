ef990553ad07ae402addabefdd297a86ade55959
Pablosarti7 <paulsarti7@gmail.com>
2023-05-27 12:53:56 -0400

    First commit for business website

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, flash
+import requests
+import smtplib
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "screensavers"
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# @app.route('/hero', methods=['POST'])
+# def hero():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         message = request.form['message']
+
+#         # Email Configuration
+#         sender_email = 'palishpy@gmail.com'
+#         receiver_email = 'your_email@example.com'
+#         password = 'vhbjakyjnuouhpyu'
+
+#         # Construct the email message
+#         subject = 'New Message from Contact Form'
+#         body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+#         message = f'Subject: {subject}\n\n{body}'
+
+#         # Send the email
+#         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+#             smtp.starttls()
+#             smtp.login(sender_email, password)
+#             smtp.sendmail(sender_email, receiver_email, message)
+
+#         return 'Message sent successfully!'
+#     else:
+#         return 'Error: Method not allowed.'
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        # Email Configuration
+        sender_email = 'palishpy@gmail.com'
+        receiver_email = 'your_email@example.com'
+        password = 'vhbjakyjnuouhpyu'
+
+        # Construct the email message
+        subject = 'New Message from Contact Form'
+        body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        message = f'Subject: {subject}\n\n{body}'
+
+        # Send the email
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.starttls()
+            smtp.login(sender_email, password)
+            smtp.sendmail(sender_email, receiver_email, message)
+
+        return 'Message sent successfully!'
+    else:
+        return 'Error!'
+
+if __name__ == '__main__':
+    app.run()
