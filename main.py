2e76609a88e35b046647116b4b0d042391b74f01
Pablosarti7 <paulsarti7@gmail.com>
2023-05-30 12:36:08 -0400

    Added jinja2, hid email and password.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request, flash
-import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
 import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] =  os.environ.get("FLASK_KEY")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET','POST'])
 def submit():
     if request.method == 'POST':
         name = request.form['name']
@@ -19,9 +21,10 @@ def submit():
         message = request.form['message']
 
         # Email Configuration
-        sender_email = ''
-        receiver_email = ''
-        password = 'vhbjakyjnuouhpyu'
+        from_email = email
+        
+        to_email = os.environ.get('MY_EMAIL')
+        to_password = os.environ.get('MY_PASSWORD')
 
         # Construct the email message
         subject = 'New Message from Contact Form'
@@ -31,14 +34,13 @@ def submit():
         # Send the email
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.starttls()
-            smtp.login(sender_email, password)
-            smtp.sendmail(sender_email, receiver_email, message)
-
-        return 'Message sent successfully!'
+            smtp.login(to_email, to_password)
+            smtp.sendmail(from_email, to_email, message)
+            
+        flash("Message Successfully Sent!")
+        return render_template("index.html")
     else:
         return 'Error!'
 
 if __name__ == '__main__':
     app.run()
-
-    
