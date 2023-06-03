04cf5bfd1079ceb078c99ec7bf5947e87090f67f
Pablosarti7 <paulsarti7@gmail.com>
2023-06-03 10:23:04 -0400

    Changes: added wtf forms

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
 import smtplib
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.urandom(32)
 
+Bootstrap(app)
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send Message")
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['GET','POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
 
         # Email Configuration
         from_email = email
-        
         to_email = os.environ.get('MY_EMAIL')
         to_password = os.environ.get('MY_PASSWORD')
 
         # Construct the email message
         subject = 'New Message from Contact Form'
         body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
         message = f'Subject: {subject}\n\n{body}'
 
         # Send the email
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.starttls()
             smtp.login(to_email, to_password)
-            smtp.sendmail(from_email, to_email, message)
-            
-        flash("Message Successfully Sent!")
-        return render_template("index.html")
-    else:
-        return 'Error!'
+            smtp.sendmail(from_addr=from_email, to_addrs=to_email, msg=message)
+
+        flash('You succesfully sent the message!')
+        return redirect(url_for('home'))
+    
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run()
